e9cd1550561276180fe1189c0b5161cc17f5673b
CodingWithR4K3 <Gorgol.Konrad@protonmail.com>
2021-02-26 12:32:22 +0100

    Creating repository

---
+++ src/main/java/com/crud/tasks/domain/TaskDto.java
@@ -0,0 +1,12 @@
+package com.crud.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TaskDto {
+    private final Long id;
+    private final String title;
+    private final String content;
+}
