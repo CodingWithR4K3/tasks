4b814791b86ac2b0b66166e9fe946db2a318481c
CodingWithR4K3 <Gorgol.Konrad@protonmail.com>
2021-03-27 21:50:49 +0100

    Module 22.4 - Exercise

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -9,29 +9,17 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/v1/trello")
-@RequiredArgsConstructor
+
 public class TrelloController {
 
     private final TrelloClient trelloClient;
 
     @GetMapping("getTrelloBoards")
-    public void getTrelloBoards() {
-        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
-        trelloBoards.forEach(trelloBoardDto -> {
-            System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName());
-
-            System.out.println("This board contains lists: ");
-
-            trelloBoardDto.getLists().forEach(trelloList -> {
-                System.out.println(
-                        trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed()
-                );
-            });
-
-        });
+    public List<TrelloBoardDto> getTrelloBoards() {
+        return trelloClient.getTrelloBoards();
     }
 
     @PostMapping("createTrelloCard")
