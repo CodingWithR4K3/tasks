107279aa52c6b43fd8e4f5320882c749d00b1b36
CodingWithR4K3 <Gorgol.Konrad@protonmail.com>
2021-06-16 17:12:02 +0200

    Module 32.2 - Exercise

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -3,9 +3,12 @@ package com.crud.tasks.service;
 import com.crud.tasks.domain.Mail;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -14,6 +17,9 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class SimpleEmailService {
+
+    @Autowired
+    private MailCreatorService mailCreatorService;
 
     private final JavaMailSender javaMailSender;
 
@@ -36,4 +42,14 @@ public class SimpleEmailService {
         mailMessage.setText(mail.getMessage());
         return mailMessage;
     }
+
+    private MimeMessagePreparator createMimeMessage(final Mail mail) {
+        return mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setTo(mail.getMailTo());
+            messageHelper.setSubject(mail.getSubject());
+            messageHelper.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()), true);
+        };
+    }
+
 }
