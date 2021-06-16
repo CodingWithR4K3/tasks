b4ac18792e4e239147082b72197dcebf8ec22bd9
CodingWithR4K3 <Gorgol.Konrad@protonmail.com>
2021-06-16 17:19:52 +0200

    Module 32.2 - Exercise

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -1,5 +1,6 @@
 package com.crud.tasks;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -7,9 +8,9 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class TasksApplication extends SpringBootServletInitializer {
 
-	//public static void main(String[] args) {
-	//SpringApplication.run(TasksApplication.class, args);
-	//}
+	public static void main(String[] args) {
+		SpringApplication.run(TasksApplication.class, args);
+	}
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
